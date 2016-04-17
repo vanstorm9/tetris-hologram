1c36f5098fb8929004eb4aefab8823b3412a04af
xplustwo <adgeria@gmail.com>
2016-04-17 02:05:44 -0400

    block spawn
    block spawn script

---
+++ Unity-files/tetris/Assets/Scripts/itemSpawn.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class itemSpawn : MonoBehaviour {
+
+	public Transform spawnPoint;
+	public float spawnTime = 2.0f;
+	public GameObject[] block;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKeyDown) {
+			SpawnBlock ();
+		}
+	}
+
+	void SpawnBlock () {
+
+		int objectIndex = Random.Range (0, block.Length);
+		Instantiate (block [objectIndex], spawnPoint.position, spawnPoint.rotation);
+	}
+}
