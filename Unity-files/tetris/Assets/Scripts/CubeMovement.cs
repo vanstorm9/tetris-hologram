9666fd1a8c6f36f6eee6ac20ff98c1b7cd33b390
vanstorm9 <antlowhur@yahoo.com>
2016-04-17 03:33:10 -0400

    Added incremental movement

--- Unity-files/tetris/Assets/Scripts/CubeMovement.cs
+++ Unity-files/tetris/Assets/Scripts/CubeMovement.cs
@@ -17,13 +17,17 @@ using System.Collections;
         void serial2Output()
         {
         if (command == "U") {
-            input = new Vector3(0, -1, 5);
+            //input = new Vector3(0, -1, 15);
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z+1);
         } else if (command == "D") {
-            input = new Vector3(0, -1, -5);
+            //input = new Vector3(0, -1, -15);
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z-1);
         } else if (command == "L") {
-            input = new Vector3(-5, -1, 0);
+            //input = new Vector3(-15, -1, 0);
+            transform.position = new Vector3(transform.position.x - 1, transform.position.y, transform.position.z);
         } else if (command == "R") {
-            input = new Vector3(5, -1, 0);
+            transform.position = new Vector3(transform.position.x + 1, transform.position.y, transform.position.z);
+            //input = new Vector3(15, -1, 0);
         } else if (command == "RTL") {
             transform.Rotate(0, 90, 0);
         }else if (command == "RTR")
@@ -40,7 +44,7 @@ using System.Collections;
         {
             command = serial_script.command;
             serial2Output();
-            //Debug.Log("Command: " + command);
+            
     }
 
 
@@ -61,16 +65,15 @@ using System.Collections;
         */
         //Controlled movement - responds to pressing up, down, left, and right arrow keys
 
-            if (counter >= 8)
+            if (counter >= 10)
             {
                 serialListener();
                 counter = 0;
             }
-            
 
-            
-            if (GetComponent<Rigidbody>().velocity.magnitude < maxSpeed)
-            //Debug.Log();
+        Debug.Log(transform.position.x);
+
+        if (GetComponent<Rigidbody>().velocity.magnitude < maxSpeed)
             {
                 GetComponent<Rigidbody>().AddForce(input*moveSpeed);
             }
