f52db0a62fd3edd8ebe51d04589a42499edf68b9
vanstorm9 <antlowhur@yahoo.com>
2016-04-16 23:56:01 -0400

    Got serial input working

---
+++ Unity-files/Serial/Assets/Scripts/SerialRead_sample.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+using System.Threading;
+
+public class SerialRead_sample : MonoBehaviour {
+
+
+    SerialPort stream = new SerialPort("COM3", 9800); //Set the port (com4) and the baud rate (9600, is standard on most devices)
+    float[] lastRot = { 0, 0, 0 }; //Need the last rotation to tell how far to spin the camera
+    Vector3 rot;
+    Vector3 offset;
+
+
+    void Start()
+    {
+        stream.Open(); //Open the Serial Stream.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string value = stream.ReadLine(); //Read the information
+        string[] vec3 = value.Split(','); //My arduino script returns a 3 part value (IE: 12,30,18)
+        if (vec3[0] != "" && vec3[1] != "" && vec3[2] != "") //Check if all values are recieved
+        {
+            rot = new Vector3(float.Parse(vec3[0]), float.Parse(vec3[1]), float.Parse(vec3[2]));
+            //Read the information and put it in a vector3
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+                                                                        Quaternion.Euler(0, rot.x, rot.y),
+                                                                        Time.deltaTime * 3);
+            //Take the vector3 and apply it to the object this script is applied.
+            stream.BaseStream.Flush(); //Clear the serial information so we assure we get new information.
+        }
+    }
+
+    void OnGUI()
+    {
+        string newString = "Connected: " + transform.eulerAngles;
+        GUI.Label(new Rect(10, 10, 300, 100), newString); //Display new values
+        GUI.Label(new Rect(10, 30, 300, 100), "\t" + rot);
+    }
+}
---
+++ Unity-files/controller/controller.ino
@@ -0,0 +1,116 @@
+//Turns on and off a light emitting diode(LED) connected to digital pin 13, when pressing a pushbutton attached to pin 2. 
+ 
+/*
+ The circuit:
+ * LED attached from pin 13 to ground 
+ * pushbutton attached to pin 2 from +5V
+ * 10K resistor attached to pin 2 from ground
+ 
+ * Note: on most Arduinos there is already an LED on the board
+ attached to pin 13.
+ 
+ 
+ This example code is in the public domain.
+ 
+ http://www.arduino.cc/en/Tutorial/Button
+ */
+
+#define LED 6
+
+const int LeftPin = 2;     // the number of the pushbutton pin
+const int ledPin =  13;      // the number of the LED pin
+
+const int RightPin = 4;  
+const int UpPin = 7;  
+const int DownPin = 3;     
+
+
+// variables will change:
+int buttonStateLeft = 0;         // variable for reading the pushbutton status
+int buttonStateRight = 0; 
+int buttonStateUp = 0; 
+int buttonStateDown = 0; 
+
+
+char byte_val;
+
+
+void initalizeDigitalRead()
+{
+    buttonStateLeft = digitalRead(LeftPin);
+    buttonStateRight = digitalRead(RightPin);
+    buttonStateUp = digitalRead(UpPin);
+    buttonStateDown = digitalRead(DownPin);
+
+}
+
+void buttonControl()
+{
+
+  
+  if (buttonStateLeft == HIGH) {     
+    // turn LED on:    
+    Serial.print("LEFT was pressed");
+    digitalWrite(ledPin, HIGH);  
+    digitalWrite(LED, HIGH);
+  } 
+
+  if(buttonStateRight == HIGH)
+  {
+    Serial.print("RIGHT was pressed");
+    digitalWrite(ledPin, HIGH);
+    digitalWrite(LED, HIGH);   
+  } else if(buttonStateUp == HIGH){
+    Serial.print("UP was pressed");
+     digitalWrite(ledPin, HIGH);  
+    digitalWrite(LED, HIGH);
+  } else if(buttonStateDown == HIGH){
+    Serial.print("DOWN was pressed");
+     digitalWrite(ledPin, HIGH);  
+    digitalWrite(LED, HIGH);
+  }else{
+    Serial.print("No imput");
+    // turn LED off:
+    digitalWrite(ledPin, LOW);
+    digitalWrite(LED, LOW); 
+  }
+
+  Serial.flush();
+
+
+  Serial.println();
+  delay(30);
+  
+  
+}
+
+
+
+void setup()
+{
+ Serial.begin(9800);
+
+  // initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);  
+      
+  // initialize the pushbutton pin as an input:
+  pinMode(LeftPin, INPUT);
+  pinMode(RightPin, INPUT);  
+  pinMode(UpPin, INPUT);  
+  pinMode(DownPin, INPUT);  
+
+
+  pinMode(LED, OUTPUT);    
+
+  
+ 
+}
+
+
+void loop(){
+
+    initalizeDigitalRead();
+    buttonControl();
+
+  
+}
