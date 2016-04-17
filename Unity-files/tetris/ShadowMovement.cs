149406e33ae181506f95775513d2ee0bb331ea7d
xplustwo <adgeria@gmail.com>
2016-04-17 00:52:43 -0400

    Poornima update

--- Unity-files/tetris/ShadowMovement.cs
+++ Unity-files/tetris/ShadowMovement.cs
@@ -4,18 +4,19 @@ using System.Collections;
 public class ShadowMovement : MonoBehaviour {
 
     public float moveSpeed;
-    private float maxSpeed = 5f;
-    private static GameObject targObj = GameObject.FindGameObjectWithTag("L-block");
-    private Vector3 targPos = new Vector3(targObj.transform.position.x, 0, targObj.transform.position.z);
+    Transform goTo;
+    //private float maxSpeed = 5f;
+    //private static GameObject targObj = GameObject.FindGameObjectWithTag("L-block");
+    //private Vector3 targPos = new Vector3(GameObject.FindGameObjectWithTag("L-block").transform.position.x, 2, GameObject.FindGameObjectWithTag("L-block").transform.position.z);
     // Use this for initialization
     void Start () {
-	
-	}
+        goTo = GameObject.Find("L-block").transform;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-
-        transform.position = Vector3.MoveTowards(transform.position, targPos, 1);
+        transform.position = new Vector3(goTo.position.x, 2, goTo.position.z);
+        //transform.position = Vector3.MoveTowards(transform.position, targPos, moveSpeed*Time.deltaTime);
         /*
         input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
         if (GetComponent<Rigidbody>().velocity.magnitude < maxSpeed)
