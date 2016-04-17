3443c7e3d0c18ab18b48e388f91c10151c5c4bf9
xplustwo <adgeria@gmail.com>
2016-04-16 22:14:26 -0400

    tetris game
    Added tetris game stuff

---
+++ Unity-files/tetris/CubeMovement.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+/*namespace BMove
+{
+
+    public class A
+    {
+        public int ind;
+
+    }
+    */
+    
+    public class CubeMovement : MonoBehaviour
+    {
+    /*
+        public A a;
+        public void moveBot(int indx)
+        {
+            a.ind = indx;
+            Start();
+            Update();
+
+        }
+
+*/
+        public float moveSpeed;
+        private float maxSpeed = 5f;
+        private Vector3 input;
+        //public Transform[] empPoints;
+        // Use this for initialization
+
+        void Start()
+        {
+            //transform.position = empPoints[0].position;
+
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            /*
+            //test the movement of the robot
+            a.ind = 2;      //should go to cubicle 2
+            transform.position = Vector3.MoveTowards(transform.position, empPoints[a.ind].position, moveSpeed * Time.deltaTime);
+            */
+            //Controlled movement - responds to pressing up, down, left, and right arrow keys
+            
+            input = new Vector3(Input.GetAxisRaw("Horizontal"), -1, Input.GetAxisRaw("Vertical"));
+            if (GetComponent<Rigidbody>().velocity.magnitude < maxSpeed)
+            //Debug.Log();
+            {
+                GetComponent<Rigidbody>().AddForce(input*moveSpeed);
+            }
+
+            // deprecated rigidbody.AddForce(input);
+
+            //print(input);
+            
+        }
+    }
+
+//}
---
+++ Unity-files/tetris/ShadowMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShadowMovement : MonoBehaviour {
+
+    public float moveSpeed;
+    private float maxSpeed = 5f;
+    private static GameObject targObj = GameObject.FindGameObjectWithTag("L-block");
+    private Vector3 targPos = new Vector3(targObj.transform.position.x, 0, targObj.transform.position.z);
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position = Vector3.MoveTowards(transform.position, targPos, 1);
+        /*
+        input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        if (GetComponent<Rigidbody>().velocity.magnitude < maxSpeed)
+        //Debug.Log();
+        {
+            GetComponent<Rigidbody>().AddForce(input * moveSpeed);
+        }
+
+        */
+
+    }
+}
